1cd91b2c50c947d8275a13a3377b7e04bd8ed394
FrendEr <frend.wong@gmail.com>
2015-07-09 01:01:06 +0800

    update

--- src/fDatepicker.js
+++ src/fDatepicker.js
@@ -8,7 +8,7 @@
  *
  * ======================================================= */
 
-!function(root, factory) { 
+!function(root, factory) {
     if (typeof define == 'function' && define.amd) {
         define(['jquery'], function($) {
             return factory();
